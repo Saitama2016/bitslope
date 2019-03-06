4f3090ede0acdd82616bfa585d97ab545e8dd07d
Vincent <garciajvincent@gmail.com>
2019-03-06 12:38:22 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <body>
+          <main>
+          <img src="https://uc.uxpin.com/files/955849/941430/FinalBitslopes_Logo_Draft_Nuggets_with_words-2-9861a1.png"></img>
+            <h2>Legal software for peak performance</h2>
+            <section className="mailChimpBox">
+              <form>
+                <input type="text" placeholder="Email"></input>
+              </form>
+              <br />
+              <button type="submit">Sign Up</button>
+            </section>
+          </main>
+        </body>
       </div>
     );
   }
